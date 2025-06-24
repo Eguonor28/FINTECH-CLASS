18c9f5a6b6f3e860bcb35cc57efd7b1e99f61faa
Eguonor Oputu <oputueguonor@gmail.com>
2025-06-24 16:07:59 +0100

    add and delete income update

--- server/controllers/incomeController.js
+++ server/controllers/incomeController.js
@@ -24,10 +24,26 @@ const addIncome = async (req, res) => {
 };
 
 //get all income
-const getAllIncome = async (req, res) => {};
+const getAllIncome = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const income = await Income.find({ userId }).sort({ date: -1 });
+    res.json(income);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 //delete income
 
-const deleteIncome = async (req, res) => {};
+const deleteIncome = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    await Income.findByIdAndDelete(req.params.id);
+    res.json({ message: "Income deleted successful" });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 
 // Export all income-related controller functions
 export { addIncome, getAllIncome, deleteIncome };
